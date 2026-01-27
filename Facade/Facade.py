38c5fc0818ee7c7036a92a141c5a0d57cb138a0a
JesusEscobr <ing.guillermo3627@gmail.com>
2026-01-27 16:37:28 -0600

    estructurales

--- Facade/Facade.py
+++ Facade/Facade.py
@@ -1,5 +1,9 @@
-class CPU: def encender(self): pass
-class Disco: def leer(self): pass
+class CPU:
+    def encender(self):
+        pass
+class Disco:
+    def leer(self):
+        pass
 
 class ComputadoraFacade:
     def iniciar(self):
--- Flyweight/Flyweight.js
+++ Flyweight/Flyweight.js
@@ -1,3 +1,17 @@
-class Flyweight {
-  constructor(type) { this.type = type; } // Compartido
+class TreeType {
+  constructor(name, texture) { this.name = name; this.texture = texture; }
 }
+
+const treeFactory = {
+  types: {},
+  get(name, texture) {
+    if (!this.types[name]) this.types[name] = new TreeType(name, texture);
+    return this.types[name];
+  }
+};
+
+const forest = [];
+const addTree = (x, y, name, tex) => forest.push({ x, y, type: treeFactory.get(name, tex) });
+
+// 10,000 árboles, pero solo 1 objeto
+for(let i=0; i<10000; i++) addTree(i, i, "Pino", "pino.jpg");
